587971fc39ced52eeade08ea6fbc9eb3e857c8d4
Nicolas Loewen <nic_loewen@yahoo.ca>
2017-11-06 17:42:16 +0000

    Added the functionality of login with username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:firstName, :lastName, :username, :password, :password_confirmation, :email])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:firstName, :lastName, :username, :email, :password, :password_confirmation])
+
+  end
+
 end
