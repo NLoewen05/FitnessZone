df4c779eb2f2b9a1bc1fc939cf9dc58fa0a4ef22
Nicolas Loewen <nic_loewen@yahoo.ca>
2017-11-06 15:48:53 +0000

    Created routes for hte home, about, and contact pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :pages
+  root to: 'pages#home', as: 'home'
+
+  get 'about', to: 'pages#about', as: 'about'
+
+  get 'contact', to: 'pages#contact', as: 'contact'
 end
