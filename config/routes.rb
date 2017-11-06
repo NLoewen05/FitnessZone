759e863158f8ea423023a7560be30b47fb9ec26b
Nicolas Loewen <nic_loewen@yahoo.ca>
2017-11-06 18:11:45 +0000

    Changed routes for heroku removing registration controller on users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations'}
+  devise_for :users, path: 'users', controllers: { sessions: "users/sessions"}
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
