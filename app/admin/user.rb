6a782e8ee6a358b19ef267cb95ebf30e1c6f5beb
Nicolas Loewen <nic_loewen@yahoo.ca>
2017-11-07 16:24:40 +0000

    Added the ability to create a user through active admin

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,4 +1,38 @@
 ActiveAdmin.register User do
   permit_params :username, :email, :firstName, :lastName, :email, :password
+
+    index do
+    selectable_column
+    id_column
+    column :email
+    column :username
+    column :firstName
+    column :lastName
+    column :current_sign_in_at
+    column :sign_in_count
+    column :created_at
+    actions
+  end
+
+  filter :email
+  filter :username
+  filter :firstName
+  filter :lastName
+  filter :current_sign_in_at
+  filter :sign_in_count
+  filter :created_at
+
+  form do |f|
+    f.inputs do
+      f.input :email
+      f.input :username
+      f.input :firstName
+      f.input :lastName
+      f.input :password
+      f.input :password_confirmation
+    end
+    f.actions
+  end
+
 end
 
