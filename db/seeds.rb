7935a28ecee72e0a2bfc9f5631361233548a11ff
Nicolas Loewen <nic_loewen@yahoo.ca>
2017-11-07 19:18:48 +0000

    Added all products view on the home page, and seeded the provinces

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+
+  provinces = ['Alberta', 'British Columbia', 'Manitoba',
+              'New Brunswick', 'Newfoundland and Labrador', 'Nova Scotia',
+              'Ontario', 'Prince Edward Island', 'Quebec', 'Saskatchewan']
+
+  provinces.each { |p| Province.create!(:name => p) }
