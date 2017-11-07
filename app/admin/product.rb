737c8361f09a41784c259406e714436e9da8e234
Nicolas Loewen <nic_loewen@yahoo.ca>
2017-11-07 18:04:20 +0000

    Changed the product display variables for active admin

--- app/admin/product.rb
+++ app/admin/product.rb
@@ -2,15 +2,15 @@ ActiveAdmin.register Product do
   permit_params :name, :weight, :price, :description, :image, :caption, :product_category_id
 
 
-form :html => { :enctype => "multipart/form-data" } do |f|
-   f.inputs "Details" do
-    f.input :name
-    f.input :weight
-    f.input :description
-    f.input :price
-    f.input :caption
-    f.input :image, :as => :file
-  end
-  f.actions
- end
+# form :html => { :enctype => "multipart/form-data" } do |f|
+#    f.inputs "Details" do
+#     f.input :name
+#     f.input :weight
+#     f.input :description
+#     f.input :price
+#     f.input :caption
+#     f.input :image, :as => :file
+#   end
+#   f.actions
+#  end
 end
