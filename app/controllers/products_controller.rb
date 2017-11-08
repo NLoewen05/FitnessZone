36702846358f42ea07955fc1271043dfd1c5e8ba
NLoewen05 <nic_loewen@yahoo.ca>
2017-11-08 05:33:52 +0000

    Edited the product controller to allow upload of file for s3 aws

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,18 @@
 class ProductsController < ApplicationController
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to @product, notice: 'Product was successfully created.'
+    else
+      render action: 'new'
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:image, :name, :weight, :description, :price, :caption)
+  end
 end
