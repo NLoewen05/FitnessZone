855187f7b02f3bb623b85e548d43e9195956530f
NLoewen05 <nic_loewen@yahoo.ca>
2017-11-22 20:04:16 +0000

    Changed some visual css and changed the pagination limit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def home
-    @products = Product.order("name").page(params[:page]).per(12)
+    @products = Product.order("name").page(params[:page]).per(4)
   end
 
   def details
@@ -16,7 +16,7 @@ class PagesController < ApplicationController
   end
 
   def categories
-    @products = Product.where('product_category_id = ?', params[:id].to_i).page(params[:page]).per(12)
+    @products = Product.where('product_category_id = ?', params[:id].to_i).page(params[:page]).per(4)
   end
 
   def search_result
@@ -25,19 +25,19 @@ class PagesController < ApplicationController
       wildcard_keywords = '%' + params[:search] + '%'
 
       if params[:category].to_i == 0
-        @products = Product.where('name LIKE ? OR description LIKE ?', wildcard_keywords, wildcard_keywords).page(params[:page]).per(12)
+        @products = Product.where('name LIKE ? OR description LIKE ?', wildcard_keywords, wildcard_keywords).page(params[:page]).per(4)
       else
-        @products = Product.where('name LIKE ? OR description LIKE ?', wildcard_keywords, wildcard_keywords).where('product_category_id = ?', params[:category].to_i).page(params[:page]).per(12)
+        @products = Product.where('name LIKE ? OR description LIKE ?', wildcard_keywords, wildcard_keywords).where('product_category_id = ?', params[:category].to_i).page(params[:page]).per(4)
       end
     end
   end
 
   def new_product
-    @products = Product.where("created_at >= ?", 2.days.ago.utc).order(:created_at).page(params[:page]).per(12)
+    @products = Product.where("created_at >= ?", 2.days.ago.utc).order(:created_at).page(params[:page]).per(4)
   end
 
   def updated_product
-    @products = Product.where("updated_at >= ?", 2.days.ago.utc).order(:updated_at).page(params[:page]).per(12)
+    @products = Product.where("updated_at >= ?", 2.days.ago.utc).order(:updated_at).page(params[:page]).per(4)
   end
 
 end
