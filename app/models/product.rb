d253430c906bd3c0869f6214f0f8fbb6e484dd57
NLoewen05 <nic_loewen@yahoo.ca>
2017-11-28 15:59:26 +0000

    Added some visual styling using cards from semantic ui

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,9 +5,12 @@ class Product < ApplicationRecord
 
   validates :name, :weight, :price, :description, :caption, presence: true
 
-  has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200" }
+  has_attached_file :image, styles: { small: "200x200>", med: "300x300>", large: "1024x1024>" }
 
   validates_attachment_file_name :image, :matches => [/\.(gif|jpg|jpeg|tiff|png)$/]
   validates_uniqueness_of :image_file_name # this is a standard ActiveRecord validator
+
+
+  paginates_per 10
 
 end
