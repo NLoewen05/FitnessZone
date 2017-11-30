56deeb381b5dfcb421c00f31cb2eceeeacc4ae97
NLoewen05 <nic_loewen@yahoo.ca>
2017-11-30 15:20:05 +0000

    Fixed the update params - missing comma

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,8 +1,11 @@
-$('.message .close')
-  .on('click', function() {
-    $(this)
-      .closest('.message')
-      .transition('fade')
-    ;
-  })
-;
+var ready = function(){
+  $('.message .close')
+    .on('click', function() {
+      $(this)
+        .closest('.message')
+        .transition('fade')
+      ;
+    })
+  ;
+}
+$(document).ready(ready);
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -52,7 +52,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
     devise_parameter_sanitizer.permit(:account_update) do |permit_params|
-      permit_params.permit(:firstName, :lastName, :username, :email, :password, :current_password, :password_confirmation
+      permit_params.permit(:firstName, :lastName, :username, :email, :password, :current_password, :password_confirmation,
       addresses_attributes: [:address_type, :address_line, :city, :province_id, :postal_code, :country])
     end
   end
